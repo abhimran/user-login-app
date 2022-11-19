ccd9b90570f8c1795ba2292944622ee6fb42972d
abhimran <abhimrann@gmail.com>
2022-11-19 12:26:20 +0600

    initialize user-login app project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
